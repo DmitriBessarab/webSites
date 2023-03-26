96ce3646ae033bb2b1c3021fd6aa4ebd384c1b28
SunBirdLT <sunbirdkl@gmail.com>
2023-03-26 16:01:17 +0300

    Qichen MAIN - integration

---
+++ Qichen MAIN/Qichen.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,103 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Qichen.Web
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+            //------STYLES--------
+            bundles.Add(new StyleBundle("~/bundles/bootstrap/css").Include(
+                   "~/Content/bootstrap.min.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/jquery-ui.theme/css").Include(
+                   "~/Content/jquery-ui.theme.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/font-awesome/css").Include(
+                   "~/Content/font-awesome.css",new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/flaticon/css").Include(
+                   "~/Content/flaticon.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/jquery/css").Include(
+                   "~/Content/jquery-ui.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/animate/css").Include(
+                   "~/Content/animate.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/magnific-popup/css").Include(
+                   "~/Content/magnific-popup.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/nice-select/css").Include(
+                   "~/Content/nice-select.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/Site/css").Include(
+                   "~/Content/Site.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/slick/css").Include(
+                   "~/Content/slick.css", new CssRewriteUrlTransform()));
+
+            //-------Custom CSS-------//
+
+            bundles.Add(new StyleBundle("~/bundles/default/css").Include(
+                   "~/Vendor/css/default.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/style/css").Include(
+                   "~/Vendor/css/style.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/_cart-checkout/css").Include(
+                   "~/Vendor/css/sections/_cart-checkout.css", new CssRewriteUrlTransform()));
+
+
+
+
+            //-------SCRIPTS--------------//
+
+            bundles.Add(new Bundle("~/bundles/jquery/js").Include(
+                "~/Scripts/jquery-3.6.4.min.js"));
+
+            bundles.Add(new Bundle("~/bundles/jquery-ui/js").Include(
+                "~/Scripts/jquery-ui-1.13.2.js"));
+
+            bundles.Add(new Bundle("~/bundles/bootstrap/js").Include(
+                "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new Bundle("~/bundles/modernizr/js").Include(
+                "~/Scripts/modernizr-2.8.3.js"));
+
+            bundles.Add(new Bundle("~/bundles/popper/js").Include(
+                "~/Scripts/popper.min.js"));
+
+            bundles.Add(new Bundle("~/bundles/slick/js").Include(
+                "~/Scripts/slick.min.js"));
+
+            bundles.Add(new Bundle("~/bundles/wow/js").Include(
+                "~/Scripts/wow.min.js"));
+
+            bundles.Add(new Bundle("~/bundles/jquery-counterup/js").Include(
+                "~/Scripts/jquery.counterup.min.js"));
+
+            bundles.Add(new Bundle("~/bundles/jquery-waypoints/js").Include(
+               "~/Scripts/jquery.waypoints.js"));
+            
+            bundles.Add(new Bundle("~/bundles/jquery-nice-select/js").Include(
+               "~/Scripts/jquery.nice-select.min.js"));
+
+            bundles.Add(new Bundle("~/bundles/jquery-nice-number/js").Include(
+               "~/Scripts/jquery.nice-number.min.js"));
+
+            bundles.Add(new Bundle("~/bundles/isotope/js").Include(
+            "~/Scripts/isotope.min.js"));
+
+            bundles.Add(new Bundle("~/bundles/imagesloaded/js").Include(
+           "~/Scripts/imagesloaded.min.js"));
+
+            //-------Custom Scripts-----------//
+            bundles.Add(new Bundle("~/bundles/theme/js").Include(
+                "~/Vendor/js/theme.js"));
+        }
+    }
+}
---
+++ Qichen MAIN/Qichen.Web/Controllers/PagesController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Qichen.Web.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Pages
+        public ActionResult Err404()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Cart()
+        {
+            return View();
+        }
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+        public ActionResult Chefs()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Faq()
+        {
+            return View();
+        }
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+        public ActionResult MenuFastFood()
+        {
+            return View();
+        }
+        public ActionResult MenuSeaFood()
+        {
+            return View();
+        }
+        public ActionResult Products()
+        {
+            return View();
+        }
+        public ActionResult Reservations()
+        {
+            return View();
+        }
+
+    }
+}
